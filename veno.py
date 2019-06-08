c398df44e93c8d3e47abe7ccfcd56ab98e291892
Devpixx <51494690+Devpixx@users.noreply.github.com>
2019-06-08 11:11:11 +0200

    Update veno.py

--- veno.py
+++ veno.py
@@ -3,7 +3,7 @@ from discord.ext import commands, tasks
 from itertools import cycle
 import os
 
-client = commands.Bot(command_prefix=["~","-"] ,description="C o d e : V")
+client = commands.Bot(command_prefix=["~","-"] ,description="V e n o")
 status = cycle(["Higefive des Monats", "Savas"])
 
 @client.event
@@ -21,7 +21,15 @@ async def G(ctx):
     await ctx.send(f"**G steht für Gönnen**")
 
 @client.command()
+async def g(ctx):
+    await ctx.send(f"**G steht für Gönnen**")
+
+@client.command()
 async def Twitch(ctx):
+    await ctx.send(f"**https://www.twitch.tv/venomenice**")
+
+@client.command()
+async def twitch(ctx):
     await ctx.send(f"**https://www.twitch.tv/venomenice**")
 
 @client.command()
@@ -29,24 +37,49 @@ async def Follower(ctx):
     await ctx.send(f"**2,9 Tsd.**")
 
 @client.command()
+async def follower(ctx):
+    await ctx.send(f"**2,9 Tsd.**")
+
+@client.command()
 async def Aufrufe(ctx):
+    await ctx.send(f"**490,0 Tsd.**")
+
+@client.command()
+async def aufrufe(ctx):
     await ctx.send(f"**490,0 Tsd.**")
 
 @client.command()
 async def Top_Clip(ctx):
     await ctx.send(f"**https://www.twitch.tv/venomenice/clip/EmpathicPowerfulWalletDoritosChip?filter=clips&range=all&sort=time**")
 
+
+@client.command()
+async def top_clip(ctx):
+    await ctx.send(f"**https://www.twitch.tv/venomenice/clip/EmpathicPowerfulWalletDoritosChip?filter=clips&range=all&sort=time**")
+
+
 @client.command()
 async def Facebook(ctx):
+    await ctx.send(f"**https://www.facebook.com/VenoTV**")
+
+@client.command()
+async def facebook(ctx):
     await ctx.send(f"**https://www.facebook.com/VenoTV**")
 
 @client.command()
 async def Twitter(ctx):
     await ctx.send(f"**https://twitter.com/Venomenice**")
 
+@client.command()
+async def twitter(ctx):
+    await ctx.send(f"**https://twitter.com/Venomenice**")
 
 @client.command()
 async def YouTube(ctx):
+    await ctx.send(f"**https://www.youtube.com/user/Venomenice**")
+
+@client.command()
+async def youtube(ctx):
     await ctx.send(f"**https://www.youtube.com/user/Venomenice**")
 
 @client.command()
@@ -61,19 +94,18 @@ async def commands(ctx):
     embed.set_image(url="https://static-cdn.jtvnw.net/jtv_user_pictures/venomenice-profile_banner-86581132f11e49c2-480.png")
     embed.set_thumbnail(url="https://static-cdn.jtvnw.net/jtv_user_pictures/venomenice-profile_banner-86581132f11e49c2-480.png")
 
-    embed.add_field(name="~G", value="value1")
-    embed.add_field(name="~Twitch", value="value1")
-    embed.add_field(name="~Twitter", value="value1")
+    embed.add_field(name="~G", value="~g")
+    embed.add_field(name="~Twitch", value="~twitch")
+    embed.add_field(name="~Twitter", value="~twitter")
 
-    embed.add_field(name="~Facebook", value="value1")
-    embed.add_field(name="~YouTube", value="value1")
+    embed.add_field(name="~Facebook", value="~facebook")
+    embed.add_field(name="~YouTube", value="~youtube")
 
-    embed.add_field(name="~Top_Clip", value="value1")
+    embed.add_field(name="~Top_Clip", value="~top_clip")
 
-    embed.add_field(name="~Follower", value="value1")
-    embed.add_field(name="~Aufrufe", value="value1")
+    embed.add_field(name="~Follower", value="~follower")
+    embed.add_field(name="~Aufrufe", value="~aufrufe")
 
 
     await ctx.send(embed=embed)
-
 client.run(os.getenv("TOKEN"))
